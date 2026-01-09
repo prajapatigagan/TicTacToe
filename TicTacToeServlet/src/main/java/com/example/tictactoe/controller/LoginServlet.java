d4327526b0eb1617be7f166263a7cde4b78c4d0e
gagan005 <168338988+prajapatigagan@users.noreply.github.com>
2026-01-10 01:43:30 +0530

    Update LoginServlet.java

--- TicTacToeServlet/src/main/java/com/example/tictactoe/controller/LoginServlet.java
+++ TicTacToeServlet/src/main/java/com/example/tictactoe/controller/LoginServlet.java
@@ -1,30 +1,3 @@
-//package com.example.tictactoe.controller;
-//
-//import java.io.IOException;
-//import com.example.tictactoe.model.UserDatabase;
-//import jakarta.servlet.*;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.*;
-//
-//@WebServlet("/login")
-//public class LoginServlet extends HttpServlet {
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//        String username = request.getParameter("username");
-//        String password = request.getParameter("password");
-//
-//        if (UserDatabase.isValidUser(username, password)) {
-//            HttpSession session = request.getSession();
-//            session.setAttribute("username", username);
-//            response.sendRedirect("playerNames.jsp");
-//        } else {
-//            response.getWriter().println("<h3 style='color:red;text-align:center;'>Invalid username or password!</h3>");
-//            request.getRequestDispatcher("login.jsp").include(request, response);
-//        }
-//    }
-//}
-
 package com.example.tictactoe.controller;
 
 import java.io.IOException;
